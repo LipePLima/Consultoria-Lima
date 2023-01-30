17121c7b79fd1d43e5343d822e6faa6d2cfe2502
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-01-30 19:21:08 -0300

    Excluindo informações de CEP inseridas no html

--- src/app/components/rastreamento/rastreamento.component.ts
+++ src/app/components/rastreamento/rastreamento.component.ts
@@ -16,9 +16,31 @@ export class RastreamentoComponent {
   public list: any
 
   public getEncomenda (rastreamento: any): void {
+    const inputErro: HTMLElement | null = document.getElementById('iRas');
+    const error    : HTMLElement | null = document.getElementById('erro');
+
+    const regexTest = /^[A-Z]{2}[1-9]{9}[A-Z]{2}$/
+
+    rastreamento = rastreamento.replace(regexTest, '')
+
+    if (rastreamento == '') {
+      this.checkCep(inputErro, error, 'Digite um código')
+
+    } else if (rastreamento.length < 13) {
+      this.checkCep(inputErro, error, 'OBRIGATÓRIO: 9 números e 4 letras')
+
+    }
+
     this.rastreamentoService.searchPackage(rastreamento).toPromise().then( (data: any) => {
       this.text = ''
       this.list = data!.eventos
     })
+  }
+
+
+  private checkCep (border: any, erro: any, text: any) {
+    erro!.textContent    = text
+    erro!.style.display  = 'initial'
+    border!.style.border = '1px solid red'
   }
 }
